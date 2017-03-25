a10c90d79347ac3af244186486278da6af43a588
3ll3n <ellengrafton01@gmail.com>
2017-03-25 12:49:00 +0000

    Added should have a title test to record_test and added title to constructor in record

--- specs/record_test.js
+++ specs/record_test.js
@@ -1,16 +1,21 @@
-var Record = require('../record');
-var assert = require('assert');
+var Record = require("../record");
+var assert = require("assert");
 
 describe('Record', function() {
 
   var record1;
+  var title;
 
   beforeEach(function() {
-    record1 = new Record("Kip Moore");
+    record1 = new Record("Kip Moore", "Up All Night");
   });
 
-  it('should have an artist', function() {
+  it("should have an artist", function() {
     assert.equal("Kip Moore", record1.artist);
   });
+
+  it("should have a title", function() {
+    assert.equal("Up All Night", record1.title);
+  })
 
 }); 
