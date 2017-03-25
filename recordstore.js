512116b2a3b7e3200059ce3a0f088b187b0f67f4
3ll3n <ellengrafton01@gmail.com>
2017-03-25 13:06:57 +0000

    Added test to recordstore_test to check empty inventory. Added empty inventory to constructor in recordstore

--- recordstore.js
+++ recordstore.js
@@ -1,6 +1,7 @@
 var Recordstore = function(name, city) {
   this.name = name;
   this.city = city;
+  this.inventory = [];
 }
 
 module.exports = Recordstore;
--- specs/recordstore_test.js
+++ specs/recordstore_test.js
@@ -1,12 +1,15 @@
 var Recordstore = require("../recordstore");
 var assert = require("assert");
+var Record = require("../record");
 
 describe("Recordstore", function() {
 
   var recordstore;
+  var record1;
 
   beforeEach(function() {
     recordstore = new Recordstore("Layla's Records", "Glasgow");
+    record1 = new Record("Kip Moore", "Up All Night", 5.99);
   });
 
   it("should have a name", function() {
@@ -15,6 +18,10 @@ describe("Recordstore", function() {
 
   it("should have a city", function() {
     assert.equal("Glasgow", recordstore.city);
+  });
+
+  it("should have an empty inventory to begin with", function() {
+    assert.equal(0, recordstore.inventory);
   });
 
 });
