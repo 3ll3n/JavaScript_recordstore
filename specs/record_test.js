ce50eae8f34b3d751de7a9e357595c8bb8a01770
3ll3n <ellengrafton01@gmail.com>
2017-03-25 12:52:02 +0000

    Added should have a price test to record_test and updated constructor in record

--- specs/record_test.js
+++ specs/record_test.js
@@ -6,7 +6,7 @@ describe('Record', function() {
   var record1;
 
   beforeEach(function() {
-    record1 = new Record("Kip Moore", "Up All Night");
+    record1 = new Record("Kip Moore", "Up All Night", 5.99);
   });
 
   it("should have an artist", function() {
@@ -16,5 +16,9 @@ describe('Record', function() {
   it("should have a title", function() {
     assert.equal("Up All Night", record1.title);
   })
+
+  it("should have a price", function() {
+    assert.equal(5.99, record1.price);
+  });
 
 }); 
